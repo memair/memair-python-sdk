c71e52837b5398d31e6b9ad35a7b8f15448bc950
gregology <greg@gho.st>
2018-10-09 11:17:13 -0400

    moving to 0.0.0 versioning as package is stable

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='memair',
-  version='2018.08.19.0',
+  version='0.1',
   description='SDK for Memair',
   long_description=open('README.rst').read(),
   url='https://github.com/memair/memair-python-sdk',
